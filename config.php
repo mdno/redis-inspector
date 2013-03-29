29d5f36127fcc2ecf4ced58eaa243129c9824ca6
Avétis KAZARIAN <avetis.k@gmail.com>
2013-03-29 14:16:46 +0100

    add settings form with session in header

--- config.php
+++ config.php
@@ -1,13 +1,37 @@
 <?php
 
-define('SEPARATOR', isset($_GET['separator']) ? $_GET['separator'] : ':');
+if (isset($_POST['reset'])) {
+	$_POST['separator'] = $_SESSION['separator'] = null;
+	$_POST['prefix'] = $_SESSION['prefix'] = null;
+	$_POST['limit'] = $_SESSION['limit'] = null;
+	$_POST['db'] = $_SESSION['db'] = null;
+	$_POST['host'] = $_SESSION['host'] = null;
+	$_POST['port'] = $_SESSION['port'] = null;
+}
 
-define('PREFIX', isset($_GET['prefix']) ? $_GET['prefix'] : '');
+/**
+ * Load config
+ */
+function get_config($name, $default = null) {
+	if (isset($_POST[$name])) {
+		return $_POST[$name];
+	}
 
-define('LIST_LIMIT', isset($_GET['limit']) ? (int) $_GET['limit'] : -1);
+	if (isset($_SESSION[$name])) {
+		return $_SESSION[$name];
+	}
 
-define('REDIS_DB', isset($_GET['db']) ? (int) $_GET['db'] : 0);
+	return $default;
+};
 
-define('REDIS_HOST', isset($_GET['host']) ? $_GET['host'] : 'localhost');
+define('SEPARATOR', $_SESSION['separator'] = get_config('separator', ':'));
 
-define('REDIS_PORT', isset($_GET['port']) ? (int) $_GET['port'] : 6379);
+define('PREFIX', $_SESSION['prefix'] = get_config('prefix', ''));
+
+define('LIST_LIMIT', $_SESSION['limit'] = (int) get_config('limit', -1));
+
+define('REDIS_DB', $_SESSION['db'] = (int) get_config('db', 0));
+
+define('REDIS_HOST', $_SESSION['host'] = get_config('host', '127.0.0.1'));
+
+define('REDIS_PORT', $_SESSION['port'] = (int) get_config('port', 6379));
--- views/layout.php
+++ views/layout.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="uft-8" />
 	<title>Redis inspector</title>
 	<link rel="stylesheet" type="text/css" href="assets/style.css" />
 </head>
 <body>
-	<h1><a href="<?php echo $_SERVER['SCRIPT_NAME']?>">Redis inspector</a></h1>
+	<header>
+		<h1><a href="<?php echo $_SERVER['SCRIPT_NAME']?>">Redis inspector</a></h1>
+		<form action="#" method="post">
+			<p class="separator"><label>separator <input type="text" name="separator" value="<?php echo get_config('separator') ?>" placeholder="separator" /></label></p>
+			<p class="prefix"><label>prefix <input type="text" name="prefix" value="<?php echo get_config('prefix') ?>" placeholder="prefix" /></label></p>
+			<p class="limit"><label>limit <input type="text" name="limit" value="<?php echo get_config('limit') ?>" placeholder="limit" /></label></p>
+			<p class="db"><label>db <input type="text" name="db" value="<?php echo get_config('db') ?>" placeholder="db" /></label></p>
+			<p class="port"><label>port <input type="text" name="port" value="<?php echo get_config('port') ?>" placeholder="port" /></label></p>
+			<p class="host"><label>host <input type="text" name="host" value="<?php echo get_config('host') ?>" placeholder="host" /></label></p>
+			<p><input type="submit" value="save" /></p>
+			<p><input type="submit" value="reset" name="reset" class="reset" /></p>
+		</form>
+	</header>
 	<?php echo $content_for_layout; ?>
 </body>
 </html>
